a9f2b367c9a5b5de3d36826e46dfb165d02eb510
Mathieu Bellavance <v4nille@proton.me>
2025-02-18 15:15:15 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const periods = ["Aujourd'hui", "Cette semaine", "Ce mois"];
+
+let selectedPeriod = ref("Aujourd'hui");
+
+function selectPeriod(period: string) {
+    console.log(period);
+    selectedPeriod.value = period;
+}
+</script>
+
+<template>
+    <nav>
+        <span>
+            <a
+                href="#"
+                v-for="period of periods"
+                v-bind:key="period"
+                v-bind:class="{ 'bg-primary': selectedPeriod === period }"
+                v-on:click="selectPeriod(period)"
+            >
+                {{ period }}
+            </a>
+        </span>
+    </nav>
+    {{ selectedPeriod }}
+</template>
+
+<style scoped>
+nav a {
+    margin: 0 10px;
+    padding: 5px;
+    border-radius: 3px;
+    color: inherit;
+    text-decoration: none;
+}
+
+nav a.bg-primary {
+    color: white;
+}
+</style>
---
+++ src/components/Timeline.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<nav>
+    <span>
+        <a href="#">Aujourd'hui</a>
+    </span>
+</nav>
+  </template>
+  
+  <style>
+
+  </style>
+  
